277843568957088bce58804c1d53e4de6d553239
fanfan <fatfan1020@gmail.com>
2020-07-09 00:02:42 +0800

    测试添加

---
+++ test/test.js
@@ -0,0 +1,16 @@
+const _bind = require("../src/index")
+
+
+Function.prototype.bind2 = _bind;
+
+
+const fn1 = function () {
+  return this
+}
+
+
+
+
+const newFn1 = fn1.bind2({name:"fatfan"}) // fn1 就是src 中的 this
+console.log(newFn1().name === "fatfan")
+
